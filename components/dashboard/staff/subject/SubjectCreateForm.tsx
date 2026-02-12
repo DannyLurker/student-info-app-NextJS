d604161be0530b41d4c0a2998472b462126a2806
DannyLurker <dannycode992@gmail.com>
2026-02-12 20:27:52 +0700

    feat: enhance subject creation form with validation and mutation handling

--- components/dashboard/staff/subject/SubjectCreateForm.tsx
+++ components/dashboard/staff/subject/SubjectCreateForm.tsx
@@ -1,13 +1,33 @@
-import { Grade, Major } from "@/lib/constants/class";
+import { Grade, GRADES, Major } from "@/lib/constants/class";
 import { SubjectType } from "@/lib/constants/subject";
 import { CreateSubjectInput } from "@/lib/utils/zodSchema";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 import React, { ChangeEvent, useState } from "react";
 
 type ConfigType = "MAJOR" | "GRADE";
 
+const grades = ["TENTH", "ELEVENTH", "TWELFTH"] as Grade[];
+const majors = ["SOFTWARE_ENGINEERING", "ACCOUNTING"] as Major[];
+const subjectTypes = ["GENERAL", "MAJOR"] as SubjectType[];
+
 const SubjectCreateForm = () => {
+  //  State management
   const [subjectData, setSubjectData] = useState<CreateSubjectInput>({
     subjectRecords: [],
+  });
+  const [errorMessage, setErrorMessage] = useState("");
+
+  /* 
+    TODO:
+    1. create onSucess, onError and loading UI
+    2. Refactor code
+  */
+
+  const mutation = useMutation({
+    mutationFn: () => {
+      return axios.post("/api/staff/subject", subjectData);
+    },
   });
 
   const addSubjectSet = () => {
@@ -66,17 +86,21 @@ const SubjectCreateForm = () => {
       const updatedSubjectRecords = prev.subjectRecords.map(
         (subject, index) => {
           if (index === subjectSetIndex) {
+            const majorLimitReached =
+              configType === "MAJOR" && subject.subjectConfig.major.length < 3;
+
+            const gradeLimitReached =
+              configType === "GRADE" && subject.subjectConfig.grade.length < 4;
+
             return {
               ...subject,
               subjectConfig: {
-                major:
-                  configType === "MAJOR"
-                    ? ([...subject.subjectConfig.major, ""] as Major[])
-                    : ([...subject.subjectConfig.major] as Major[]),
-                grade:
-                  configType === "GRADE"
-                    ? ([...subject.subjectConfig.grade, ""] as Grade[])
-                    : ([...subject.subjectConfig.grade] as Grade[]),
+                major: majorLimitReached
+                  ? ([...subject.subjectConfig.major, ""] as Major[])
+                  : ([...subject.subjectConfig.major] as Major[]),
+                grade: gradeLimitReached
+                  ? ([...subject.subjectConfig.grade, ""] as Grade[])
+                  : ([...subject.subjectConfig.grade] as Grade[]),
                 subjectType: subject.subjectConfig.subjectType as SubjectType,
               },
             };
@@ -194,6 +218,45 @@ const SubjectCreateForm = () => {
     });
   };
 
+  const deleteSubjectConfig = (
+    subjectSetIndex: number,
+    configType: ConfigType,
+    subjectConfigIndex: number,
+  ) => {
+    setSubjectData((prev) => {
+      const updatedSubjectData = subjectData.subjectRecords.map(
+        (subject, index) => {
+          if (index === subjectSetIndex) {
+            if (configType == "GRADE") {
+              return {
+                ...subject,
+                subjectConfig: subject.subjectConfig.grade.filter(
+                  (_, gradeIndex) => subjectConfigIndex !== gradeIndex,
+                ),
+              };
+            }
+
+            if (configType == "MAJOR") {
+              return {
+                ...subject,
+                subjectConfig: subject.subjectConfig.major.filter(
+                  (_, majorIndex) => subjectConfigIndex !== majorIndex,
+                ),
+              };
+            }
+          }
+
+          return subject;
+        },
+      );
+
+      return {
+        ...prev,
+        updatedSubjectData,
+      };
+    });
+  };
+
   const deleteSubjectName = (
     subjectSetIndex: number,
     subjectNameIndex: number,
@@ -215,6 +278,39 @@ const SubjectCreateForm = () => {
         updatedSubject,
       };
     });
+  };
+
+  const handleSubmit = () => {
+    // Frontend validation, All fields must be filled
+    subjectData.subjectRecords.map((subject, index) => {
+      if (subject.subjectNames.length === 0) {
+        setErrorMessage(
+          `Subject set ${index + 1}: Subject name must be filled`,
+        );
+        return;
+      }
+
+      Object.entries(subject.subjectConfig).map(([key, value]) => {
+        if (Array.isArray(value)) {
+          if (value.length === 0) {
+            setErrorMessage(
+              `Subject set ${index + 1} at ${key}: All config must be filled`,
+            );
+            return;
+          }
+          return;
+        }
+
+        if (value.length !== 0 && value !== null) {
+          setErrorMessage(
+            `Subject set ${index + 1} at ${key}: Subject type must be filled`,
+          );
+          return;
+        }
+      });
+    });
+
+    mutation.mutate();
   };
 
   return <div></div>;
